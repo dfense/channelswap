2ef766bf0d000e7c85fa4eb0c702011452cde8d9
John Frailey <jfrailey@cree.com>
2019-08-21 16:26:37 -0400

    remove unused func

--- service/channel.go
+++ service/channel.go
@@ -4,27 +4,6 @@ import (
 	"fmt"
 )
 
-// this mimics the gRPCsubstation
-// func GrpcInbound(timesToWrite int, output chan<- string, quit <-chan interface{}) {
-
-// Q:
-// 	for i := 0; i < 10; i++ {
-// 		select {
-
-// 		case <-quit:
-// 			break Q
-
-// 		default:
-// 			message := fmt.Sprintf("grpcInbound %d", i)
-// 			output <- message
-// 			time.Sleep(time.Second * 1)
-// 		}
-
-// 	}
-
-// 	fmt.Println("leaving grpcInbound")
-// }
-
 // this mimics the core reader
 func CoreReader(reader <-chan string, quit <-chan interface{}) {
 
